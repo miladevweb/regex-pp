3ce8465733c55d5d0ba2afe23f5ec876aa228da4
miladevweb <miladevweb@outlook.com>
2024-12-17 14:34:24 -0500

    docs: create flags file

---
+++ src/02flags.ts
@@ -0,0 +1,21 @@
+// g - Global search
+const regexGlobal = /cat/g
+console.log(regexGlobal.test('cat')) // true
+console.log(regexGlobal.test('dog')) // false
+console.log(regexGlobal.test("there're a lot of cats here")) // true
+
+console.log('-----')
+
+// i - Case insensitive
+const regexCaseInsensitive = /cat/i
+console.log(regexCaseInsensitive.test('CAT')) // true
+console.log(regexCaseInsensitive.test('cat')) // true
+console.log(regexCaseInsensitive.test('Cat')) // true
+console.log(regexCaseInsensitive.test('CATS')) // false
+
+console.log('-----')
+
+// m - Multiline
+const regexMultiline = /cat/m
+console.log(regexMultiline.test('cat')) // true
+console.log(regexMultiline.test('dog\ncat\ndog')) // true
